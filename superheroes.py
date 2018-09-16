dee000ed5097a2d5b95f65c34080ec609f9c74dc
Erica Naglik <erica2605@live.com>
2018-09-15 18:51:54 -0700

    first commit

---
+++ superheroes.py
@@ -0,0 +1,21 @@
+class Ability:
+    def init(self, name, attack_strength)
+
+    def attack(self):
+        # Return attack value
+
+    def update_attack(self, attack_strength):
+        # Update attack value
+
+class Hero:
+    def init(self, name):
+        # Initialize starting values
+
+    def add_ability(self, ability):
+        # Add ability to abilities list
+
+    def attack(self):
+        # Run attack() on every ability hero has
+
+        if name == "main":
+        # If you run this file from the terminal this block is executed.
